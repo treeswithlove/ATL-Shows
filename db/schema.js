93789c6fb0ffe19d77aeaa162753654687d69b1d
KC <khalifbcooper@outlook.com>
2017-09-28 15:47:21 -0400

    created venues db, models, schema

---
+++ db/schema.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+//Schema for Events will be created
+const EventSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    address:{
+        type: String,
+        required: true
+    },
+    cost:{
+        type:Number,
+        required: true
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+        required: true
+    }
+})
+
+//Schema for how all models for venues will be created
+const VenueSchema = new Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    events: [EventSchema]
+})
+
+
+//create models for each Schema
+
+const VenueModel = mongoose.model('Venue', VenueSchema);
+const EventModel = mongoose.model('Event', EventSchema);
+
+
+module.exports = {
+    VenueModel : VenueModel,
+    EventModel : EventModel
+}
+
+
---
+++ db/seeds.js
@@ -0,0 +1,49 @@
+require('dotenv').config();
+
+// Database setup
+const mongoose = require('mongoose');
+
+// Pull in Models from the `schema.js`
+const Schema = require("./schema.js");
+const db = mongoose.connection;
+
+mongoose.connect(process.env.MONGODB_URI)
+
+// Will log an error if db can't connect to MongoDB, and log 'connected to mongoDB' if success
+db.on('error', ((err) =>{console.log(err);
+})).once('open',(() => {console.log("Connected to MongoDB!");
+    }));
+
+//Sets schema model to variables    
+const VenueModel = Schema.VenueModel;
+const EventModel = Schema.EventModel;
+
+//create a theatre for the Venue Model
+const allainceTheatre = new VenueModel({name: 'The Alliance Theater'});
+
+//create an event for the Event Model
+const pippinMusical = new EventModel({name:'Pippin Musical', address:'1280 Peachtree St NE, Atlanta, GA 30309',cost:100, date: Date.now()})
+
+
+//assign events to venues
+const venues = [allainceTheatre];
+const events = [pippinMusical];
+
+
+venues.forEach((venue) => {
+    venue.events = events;
+    venue.save()
+        .then((venues) => {
+            console.log(`${venue.name} saved`);
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+});
+ 
+
+//disconnect db
+db.close();
+
+
+
